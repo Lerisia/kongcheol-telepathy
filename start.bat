7337b7518ea9ff292953ca06357b3814e756c355
spider-plant-641 <lulosecal@gmail.com>
2025-06-26 11:25:27 +0900

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,15 +1,14 @@
 @echo off
-REM 관리자 권한으로 실행되었는지 확인
+REM admin check
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo 관리자 권한으로 다시 실행합니다...
+    echo execute in admin mode
     powershell -Command "Start-Process '%~f0' -Verb runAs"
     exit /b
 )
 
 cd /d "%~dp0"
 
-REM 가상환경의 main.py 실행
 call .venv\Scripts\activate.bat
 python content/main.py
 
