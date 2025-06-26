f7a553ddecdff84f45427c600f8782eb449a2111
spider-plant-641 <lulosecal@gmail.com>
2025-06-26 11:23:41 +0900

    Update setting.bat

--- setting.bat
+++ setting.bat
@@ -1,14 +1,11 @@
 @echo off
-REM 가상환경 생성
 pip install virtualenv
 
 python -m venv .venv
 
-REM 가상환경의 pip로 requirements 설치
 call .venv\Scripts\activate.bat
 python -m pip install -r requirements.txt
 
-REM 완료 메시지
 echo.
-echo 설치 완료!
+echo install fin
 pause
